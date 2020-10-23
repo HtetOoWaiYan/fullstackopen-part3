bcd62c1a7440dc926e4a4b7d03d6ba219510c583
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2020-10-23 22:22:05 +0630

    Create database with MongoDB Atlas using Mongoose

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3  || process.argv.length > 5) {
+    console.log('Add      : node mongo.js [password] [name] [number]')
+    console.log('Retrieve : node mongo.js [password]')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+    `mongodb+srv://htetoowaiyan:${password}@cluster0.5qmkz.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
