7992d930cea3b423bb1826faaac9de77e52c14f5
HtetOoWaiYan <htetoo.waiyan@outlook.com>
2020-11-05 19:21:27 +0630

    Add the rest of Part3_d exercises

--- index.js
+++ index.js
@@ -6,45 +6,24 @@ const app = express()
 const Person = require('./models/person')
 
 // Use middlewares
-app.use(express.static('build'))
-app.use(express.json()) // json-parser
 app.use(cors())
+app.use(express.json()) // json-parser
+app.use(express.static('build'))
 
 // Use morgan middleware
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
-    time = new Date()
-    responseText = `
-        <p>Phonebook has info of ${persons.length} people</p>
-        <p>${time}</p>
-    `
-    response.send(responseText)
+    Person.find({}).then(persons => {
+        time = new Date()
+        responseText = `
+            <p>Phonebook has info of ${persons.length} people</p>
+            <p>${time}</p>
+            `
+        response.send(responseText)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -53,22 +32,24 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.find(person => person.id === id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -84,11 +65,14 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    // if (persons.find(person => person.name === body.name)) {
-    //     return response.status(409).json({
-    //         error: 'name already exists'
-    //     })
-    // }
+    Person.find({}).then(persons => {
+        if (persons.find(person => person.name === body.name)) {
+            return response.status(409).json({
+                error: 'name already exists'
+            })
+        }
+
+    })
 
     const person = new Person({
         name: body.name,
@@ -97,8 +81,35 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })    
+    })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+       name: body.name,
+       number: body.number 
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformated id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
